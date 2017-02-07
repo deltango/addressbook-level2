657b6630c0943bb36746dcd9a76b4ed192df7c5f
Denver Tan <revned.nat90@gmail.com>
2017-02-08 06:11:54 +0800

    [T4A3][W13-A4] Modified Address class to inherit from Contact class.

--- src/seedu/addressbook/data/person/Contact.java
+++ src/seedu/addressbook/data/person/Contact.java
@@ -5,20 +5,23 @@ import seedu.addressbook.data.exception.IllegalValueException;
 public class Contact {
     public final String value;
     private boolean isPrivate;
-    
-    public Contact(String value, boolean isPrivate, String messageContactConstraints, String contactValidationRegex) throws IllegalValueException {
+
+    public Contact(String value, boolean isPrivate,
+            String messageContactConstraints, String contactValidationRegex)
+            throws IllegalValueException {
         this.isPrivate = isPrivate;
         String trimmedContact = value.trim();
-        if (!isValidContact(trimmedContact,contactValidationRegex)) {
+        if (!isValidContact(trimmedContact, contactValidationRegex)) {
             throw new IllegalValueException(messageContactConstraints);
         }
         this.value = trimmedContact;
     }
-    
+
     /**
      * Checks if a given string is a valid Contact.
      */
-    public static boolean isValidContact(String test, String contactValidationRegex) {
+    public static boolean isValidContact(String test,
+            String contactValidationRegex) {
         return test.matches(contactValidationRegex);
     }
 
@@ -33,7 +36,7 @@ public class Contact {
                 || (other instanceof Contact // instanceof handles nulls
                 && this.value.equals(((Contact) other).value)); // state check
     }
-    
+
     @Override
     public int hashCode() {
         return value.hashCode();
