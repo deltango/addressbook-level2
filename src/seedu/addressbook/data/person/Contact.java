e29011b2adc5fd2a0940cde597ed69e17527306f
Denver Tan <revned.nat90@gmail.com>
2017-02-08 06:05:36 +0800

    [T4A3][W13-A4] Created Super Class Contact

---
+++ src/seedu/addressbook/data/person/Contact.java
@@ -0,0 +1,45 @@
+package seedu.addressbook.data.person;
+
+import seedu.addressbook.data.exception.IllegalValueException;
+
+public class Contact {
+    public final String value;
+    private boolean isPrivate;
+    
+    public Contact(String value, boolean isPrivate, String messageContactConstraints, String contactValidationRegex) throws IllegalValueException {
+        this.isPrivate = isPrivate;
+        String trimmedContact = value.trim();
+        if (!isValidContact(trimmedContact,contactValidationRegex)) {
+            throw new IllegalValueException(messageContactConstraints);
+        }
+        this.value = trimmedContact;
+    }
+    
+    /**
+     * Checks if a given string is a valid Contact.
+     */
+    public static boolean isValidContact(String test, String contactValidationRegex) {
+        return test.matches(contactValidationRegex);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Contact // instanceof handles nulls
+                && this.value.equals(((Contact) other).value)); // state check
+    }
+    
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+}
