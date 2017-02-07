173e1c29355510d6398480034c7e1e84f717d455
Denver Tan <revned.nat90@gmail.com>
2017-02-08 06:40:52 +0800

    [T4A4][W13-A4] Added getPrintableString in Person class that depends on the Interface.

--- src/seedu/addressbook/data/person/Person.java
+++ src/seedu/addressbook/data/person/Person.java
@@ -8,7 +8,7 @@ import java.util.Objects;
  * Represents a Person in the address book.
  * Guarantees: details are present and not null, field values are validated.
  */
-public class Person implements ReadOnlyPerson {
+public class Person implements ReadOnlyPerson{
 
     private Name name;
     private Phone phone;
@@ -82,6 +82,15 @@ public class Person implements ReadOnlyPerson {
     @Override
     public String toString() {
         return getAsTextShowAll();
+    }
+
+    public String getPrintableString(Person p) {
+        String temp = "";
+        temp += p.getName().getPrintableString() + " ";
+        temp += p.getEmail().getPrintableString() + " ";
+        temp += p.getAddress().getPrintableString() + " ";
+        temp += p.getPhone().getPrintableString();
+        return temp; 
     }
 
 }
